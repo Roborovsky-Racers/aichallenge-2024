5c89098b5ac8003f681f0aad30dd272278f6562c
Takashi Sato <t.sato17123@gmail.com>
2024-07-11 10:11:26 +0900

    create custom rviz config and change to use it in run_evaluation.bash

--- aichallenge/run_evaluation.bash
+++ aichallenge/run_evaluation.bash
@@ -21,7 +21,8 @@ sleep 20
 
 # Start Autoware
 echo "Start Autoware"
-ros2 launch aichallenge_system_launch aichallenge_system.launch.xml >autoware.log 2>&1 &
+# ros2 launch aichallenge_system_launch aichallenge_system.launch.xml >autoware.log 2>&1 &
+ros2 launch aichallenge_system_launch aichallenge_system.launch.xml rviz_config:=$(ros2 pkg prefix aichallenge_system_launch)/share/aichallenge_system_launch/config/autoware.rviz.custom >autoware.log 2>&1 &
 PID_AUTOWARE=$!
 sleep 10
 
