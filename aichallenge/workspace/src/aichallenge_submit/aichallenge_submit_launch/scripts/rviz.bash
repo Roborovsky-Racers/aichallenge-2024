c8d019a0c034a25acf69b71e474d9902fdd27ae4
Kenta Kato <kenta.kato.4321@gmail.com>
2024-08-17 02:04:35 +0900

    rviz2 起動引数を更新

--- aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/scripts/rviz.bash
+++ aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/scripts/rviz.bash
@@ -1,3 +1,3 @@
 #!/bin/bash
 pkill -9 rviz2
-ros2 run rviz2 rviz2 -d /aichallenge/workspace/src/aichallenge_submit/aichallenge_submit_launch/config/autoware.rviz.custom -s $(find-pkg-share autoware_launch)/rviz/image/autoware.png
+ros2 run rviz2 rviz2 -d $(ros2 pkg prefix aichallenge_submit_launch)/share/aichallenge_submit_launch/config/autoware.rviz.custom
