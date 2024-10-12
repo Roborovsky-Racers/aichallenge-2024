1af08d7e1ae7afbe9dabc745b746235c49bd1f83
Takashi Sato <t.sato17123@gmail.com>
2024-10-12 15:45:29 +0900

    add multicast settings to run_zenoh

--- vehicle/run_zenoh.bash
+++ vehicle/run_zenoh.bash
@@ -1,4 +1,8 @@
 #!/bin/bash
+
+sudo ip link set multicast on lo
+sudo sysctl -w net.core.rmem_max=2147483647 >/dev/null
+
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 docker run --rm \
     --net=host \
