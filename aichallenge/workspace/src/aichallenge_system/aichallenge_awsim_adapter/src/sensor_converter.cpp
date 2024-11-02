0c2fba741ef4470983bd8ea2759275821fee7e4d
Takashi Sato <t.sato17123@gmail.com>
2024-11-02 17:52:59 +0900

    gnss delay 関連の再実装

--- aichallenge/workspace/src/aichallenge_system/aichallenge_awsim_adapter/src/sensor_converter.cpp
+++ aichallenge/workspace/src/aichallenge_system/aichallenge_awsim_adapter/src/sensor_converter.cpp
@@ -13,6 +13,10 @@
 // limitations under the License.
 
 #include "sensor_converter.hpp"
+#include <chrono>
+#include <geometry_msgs/msg/detail/pose_with_covariance_stamped__struct.hpp>
+#include <limits>
+#include <thread>
 
 // namespace {
 // void normalize_quaternion(geometry_msgs::msg::Quaternion & q)
@@ -36,16 +40,19 @@ SensorConverter::SensorConverter(const rclcpp::NodeOptions & node_options)
   using std::placeholders::_1;
 
   // Parameters
-  gnss_value_uptate_period_ = declare_parameter<double>("gnss_value_uptate_period");
-  gnss_value_uptate_period_mean_ = declare_parameter<double>("gnss_value_uptate_period_mean");
-  gnss_value_uptate_period_stddev_ = declare_parameter<double>("gnss_value_uptate_period_stddev");
-  next_gnss_value_uptate_period_ = gnss_value_uptate_period_;
+  gnss_pose_cov_value_uptate_period_ = declare_parameter<double>("gnss_pose_cov_value_uptate_period");
+  gnss_pose_cov_value_uptate_period_mean_ = declare_parameter<double>("gnss_pose_cov_value_uptate_period_mean");
+  gnss_pose_cov_value_uptate_period_stddev_ = declare_parameter<double>("gnss_pose_cov_value_uptate_period_stddev");
 
-  gnss_pose_delay_ = declare_parameter<int>("gnss_pose_delay");
-  gnss_pose_cov_delay_ = declare_parameter<int>("gnss_pose_cov_delay");
+  gnss_pose_cov_delay_ = declare_parameter<double>("gnss_pose_cov_delay");
   gnss_pose_cov_delay_mean_ = declare_parameter<double>("gnss_pose_cov_delay_mean");
   gnss_pose_cov_delay_stddev_ = declare_parameter<double>("gnss_pose_cov_delay_stddev");
 
+  gnss_pose_cov_publish_period_ = declare_parameter<double>("gnss_pose_cov_publish_period");
+  gnss_pose_cov_publish_period_mean_ = declare_parameter<double>("gnss_pose_cov_publish_period_mean");
+  gnss_pose_cov_publish_period_stddev_ = declare_parameter<double>("gnss_pose_cov_publish_period_stddev");
+
+  gnss_pose_delay_ = declare_parameter<int>("gnss_pose_delay");
   gnss_pose_mean_ = declare_parameter<double>("gnss_pose_mean");
   gnss_pose_stddev_ = declare_parameter<double>("gnss_pose_stddev");
   gnss_pose_cov_mean_ = declare_parameter<double>("gnss_pose_cov_mean");
@@ -87,46 +94,139 @@ SensorConverter::SensorConverter(const rclcpp::NodeOptions & node_options)
   imu_ang_distribution_ = std::normal_distribution<double>(imu_ang_mean_, imu_ang_stddev_);
   imu_ori_distribution_ = std::normal_distribution<double>(imu_ori_mean_, imu_ori_stddev_);
   steering_angle_distribution_ = std::normal_distribution<double>(steering_angle_mean_, steering_angle_stddev_);
-  gnss_update_period_distribution_ = std::normal_distribution<double>(gnss_value_uptate_period_mean_, gnss_value_uptate_period_stddev_);
+
+  gnss_pose_cov_value_update_period_distribution_ = std::normal_distribution<double>(gnss_pose_cov_value_uptate_period_mean_, gnss_pose_cov_value_uptate_period_stddev_);
   gnss_pose_cov_delay_distribution_ = std::normal_distribution<double>(gnss_pose_cov_delay_mean_, gnss_pose_cov_delay_stddev_);
-// next_gnss_pose_cov_delay_
+  gnss_pose_cov_publish_period_distribution_ = std::normal_distribution<double>(gnss_pose_cov_publish_period_mean_, gnss_pose_cov_publish_period_stddev_);
+
+  gnss_cov_pub_thread_ = std::thread(std::bind(&SensorConverter::gnss_cov_update_and_publish_loop_, this));
+}
+
+SensorConverter::~SensorConverter() {
+  if (gnss_cov_pub_thread_.joinable()) {
+    gnss_cov_pub_thread_.join();
+  }
 }
 
 void SensorConverter::on_outlier_gnss_pose(const PoseStamped::ConstSharedPtr msg) {
   outlier_gnss_pose_ = *msg;
 }
 
+void SensorConverter::gnss_cov_update_and_publish_loop_() {
+
+  const auto get_next_gnss_pose_cov_value_uptate_period = [this]() {
+    const auto v = std::max(
+      gnss_pose_cov_value_uptate_period_,
+      gnss_pose_cov_value_uptate_period_ + gnss_pose_cov_value_update_period_distribution_(generator_));
+    RCLCPP_DEBUG(get_logger(), "Next GNSS value update period: %f", v);
+    return v;
+  };
+
+  const auto get_next_gnss_pose_cov_delay = [this]() {
+    const auto v = std::max(
+      gnss_pose_cov_delay_,
+      gnss_pose_cov_delay_ + gnss_pose_cov_delay_distribution_(generator_));
+    RCLCPP_DEBUG(get_logger(), "Next GNSS pose cov delay: %f", v);
+    return v;
+  };
+
+  const auto get_next_gnss_pose_cov_publish_period = [this]() {
+    const auto v = std::max(
+      gnss_pose_cov_publish_period_,
+      gnss_pose_cov_publish_period_ + gnss_pose_cov_publish_period_distribution_(generator_));
+    // RCLCPP_DEBUG(get_logger(), "Next GNSS pose cov publish period: %f", v);
+    return v;
+  };
+
+  const auto pop_nearest_gnss_cov = [this](const auto& stamp) -> std::optional<geometry_msgs::msg::PoseWithCovarianceStamped> {
+      std::lock_guard<std::mutex> lock(pose_cov_queue_mutex_);
+
+      if (pose_cov_queue_.empty()) {
+          return std::nullopt;
+      }
+
+      auto nearest_gnss_cov = pose_cov_queue_.front();
+      pose_cov_queue_.pop_front();
+
+      double min_diff_time = std::abs((stamp - nearest_gnss_cov.header.stamp).seconds());
+
+      while (!pose_cov_queue_.empty()) {
+          const auto current_cov = pose_cov_queue_.front();
+          const double diff_time = std::abs((stamp - current_cov.header.stamp).seconds());
+
+          if (diff_time > min_diff_time) {
+              break;
+          }
+
+          nearest_gnss_cov = current_cov;
+          min_diff_time = diff_time;
+          pose_cov_queue_.pop_front();
+      }
+
+      return nearest_gnss_cov;
+  };
+
+  const auto publish_gnss_cov_with_now_stamp = [this](const auto& gnss_cov_msg) {
+      auto msg = gnss_cov_msg;
+      msg.header.stamp = get_clock()->now();
+      pub_gnss_pose_cov_->publish(msg);
+  };
+
+  const auto to_nanoseconds = [](double seconds) -> std::chrono::nanoseconds  {
+    auto duration_in_seconds = std::chrono::duration<double>(seconds);
+    return std::chrono::duration_cast<std::chrono::nanoseconds>(duration_in_seconds);
+  };
+
+  std::optional<geometry_msgs::msg::PoseWithCovarianceStamped> last_gnss_pose_cov;
+  double next_gnss_value_uptate_period = get_next_gnss_pose_cov_value_uptate_period();
+  double next_gnss_pose_cov_delay = get_next_gnss_pose_cov_delay();
+
+  auto prev_time = get_clock()->now();
+
+  while(rclcpp::ok()) {
+    // Wait until the next publish period
+#if 1
+    rclcpp::WallRate(1.0 / get_next_gnss_pose_cov_publish_period()).sleep();
+#else
+    {
+      const auto loop_start = get_clock()->now();
+      const auto next_wakeup_time = prev_time + to_nanoseconds(get_next_gnss_pose_cov_publish_period());
+      const auto sleep_time = (next_wakeup_time - loop_start).to_chrono<std::chrono::nanoseconds>();
+      if (sleep_time > std::chrono::nanoseconds(0)) {
+        RCLCPP_WARN(get_logger(), "Sleep for %f", sleep_time.count() / 1e9);
+        rclcpp::sleep_for(sleep_time);
+      }
+      prev_time = loop_start;
+    }
+#endif
+
+    const auto now = get_clock()->now();
+
+    // If the pose is older than the value update period, publish the last updated pose
+    if(last_gnss_pose_cov.has_value() &&
+      (now - last_gnss_pose_cov->header.stamp).seconds() < next_gnss_value_uptate_period + next_gnss_pose_cov_delay) {
+      publish_gnss_cov_with_now_stamp(*last_gnss_pose_cov);
+      continue;
+    }
+
+    // Calculate the timestamp to get past data considering the delay of GNSS
+    next_gnss_pose_cov_delay = get_next_gnss_pose_cov_delay();
+    const auto past_stamp_with_delay = now - to_nanoseconds(next_gnss_pose_cov_delay);
+
+    // Try to get the nearest past GNSS pose covariance
+    if(const auto nearest_past_pose_cov = pop_nearest_gnss_cov(past_stamp_with_delay)) {
+      // If the nearest pose got, update and publish the pose
+      last_gnss_pose_cov = *nearest_past_pose_cov;
+      publish_gnss_cov_with_now_stamp(*last_gnss_pose_cov);
+
+      // Update the next value update period
+      next_gnss_value_uptate_period = get_next_gnss_pose_cov_value_uptate_period();
+    }
+  }
+}
+
 void SensorConverter::on_gnss_pose(const PoseStamped::ConstSharedPtr msg)
 {
-#if 1
-  const auto process_gnss = [this, msg]() {
-    auto noised_pose = std::make_shared<PoseStamped>(*msg);
-    noised_pose->header.stamp = now();
-    noised_pose->pose.position.x += pose_distribution_(generator_);
-    noised_pose->pose.position.y += pose_distribution_(generator_);
-    noised_pose->pose.position.z += pose_distribution_(generator_);
-    noised_pose->pose.orientation.x += pose_distribution_(generator_);
-    noised_pose->pose.orientation.y += pose_distribution_(generator_);
-    noised_pose->pose.orientation.z += pose_distribution_(generator_);
-    noised_pose->pose.orientation.w += pose_distribution_(generator_);
-    // normalize_quaternion(noised_pose->pose.orientation);
-    return noised_pose;
-  };
-
-  // If the pose is older than the value update period, update the pose
-  if (pose_ == nullptr || (now() - pose_->header.stamp).seconds() >= gnss_value_uptate_period_) {
-    pose_ = process_gnss();
-  }
-
-  const auto delayed_publish = [this](PoseStamped pose) {
-    rclcpp::sleep_for(std::chrono::milliseconds(gnss_pose_delay_));
-    pose.header.stamp = now();
-    pub_gnss_pose_->publish(pose);
-  };
-
-  std::thread delayed_publish_thread(std::bind(delayed_publish, *pose_));
-  delayed_publish_thread.detach();
-#else
   auto process_and_publish_gnss = [this, msg]() {
     rclcpp::sleep_for(std::chrono::milliseconds(gnss_pose_delay_));
     
@@ -145,12 +245,14 @@ void SensorConverter::on_gnss_pose(const PoseStamped::ConstSharedPtr msg)
 
   std::thread processing_thread(process_and_publish_gnss);
   processing_thread.detach();
-#endif
 }
 
 void SensorConverter::on_gnss_pose_cov(const PoseWithCovarianceStamped::ConstSharedPtr msg)
 {
 #if 1
+  std::lock_guard<std::mutex> lock(pose_cov_queue_mutex_);
+  pose_cov_queue_.push_back(*msg);
+#elif 0
   const auto process_gnss_cov = [this, msg]() {
     auto noised_pose_cov = std::make_shared<PoseWithCovarianceStamped>(*msg);
     noised_pose_cov->header.stamp = now();
@@ -200,7 +302,7 @@ void SensorConverter::on_gnss_pose_cov(const PoseWithCovarianceStamped::ConstSha
   delayed_publish_thread.detach();
 #else
     auto process_and_publish_gnss_cov = [this, msg]() {
-    rclcpp::sleep_for(std::chrono::milliseconds(gnss_pose_cov_delay_));
+    rclcpp::sleep_for(std::chrono::milliseconds(gnss_pose_delay_));
     
     auto pose_cov = std::make_shared<PoseWithCovarianceStamped>(*msg);
     pose_cov->header.stamp = now();
--- aichallenge/workspace/src/aichallenge_system/aichallenge_awsim_adapter/src/sensor_converter.hpp
+++ aichallenge/workspace/src/aichallenge_system/aichallenge_awsim_adapter/src/sensor_converter.hpp
@@ -16,6 +16,7 @@
 #define AUTOWARE_EXTERNAL_CMD_CONVERTER__NODE_HPP_
 
 
+#include <geometry_msgs/msg/detail/pose_with_covariance_stamped__struct.hpp>
 #include <random>
 #include <deque>
 #include <rclcpp/rclcpp.hpp>
@@ -34,6 +35,7 @@ class SensorConverter : public rclcpp::Node
 {
 public:
   explicit SensorConverter(const rclcpp::NodeOptions & node_options);
+  ~SensorConverter();
 
 private:
   rclcpp::Subscription<PoseStamped>::SharedPtr sub_outlier_gnss_pose_;
@@ -47,6 +49,7 @@ private:
   rclcpp::Publisher<SteeringReport>::SharedPtr pub_steering_report_;
 
 
+  void gnss_cov_update_and_publish_loop_();
   void on_outlier_gnss_pose(const PoseStamped::ConstSharedPtr msg);
   void on_gnss_pose(const PoseStamped::ConstSharedPtr msg);
   void on_gnss_pose_cov(const PoseWithCovarianceStamped::ConstSharedPtr msg);
@@ -59,8 +62,10 @@ private:
   Imu::SharedPtr imu_;
   SteeringReport::SharedPtr steering_report_;
   int gnss_pose_delay_;
-  int gnss_pose_cov_delay_;
-  int next_gnss_pose_cov_delay_;
+
+  std::mutex pose_cov_queue_mutex_;
+  std::deque<PoseWithCovarianceStamped> pose_cov_queue_;
+  std::thread gnss_cov_pub_thread_;
 
   std::mt19937 generator_;
   std::normal_distribution<double> pose_distribution_;
@@ -69,20 +74,26 @@ private:
   std::normal_distribution<double> imu_ang_distribution_;
   std::normal_distribution<double> imu_ori_distribution_;
   std::normal_distribution<double> steering_angle_distribution_;
-  std::normal_distribution<double> gnss_update_period_distribution_;
-  std::normal_distribution<double> gnss_pose_cov_delay_distribution_;
 
-  double gnss_value_uptate_period_;
-  double gnss_value_uptate_period_mean_;
-  double gnss_value_uptate_period_stddev_;
-  double next_gnss_value_uptate_period_;
+  double gnss_pose_cov_publish_period_;
+  double gnss_pose_cov_publish_period_mean_;
+  double gnss_pose_cov_publish_period_stddev_;
+  std::normal_distribution<double> gnss_pose_cov_publish_period_distribution_;
+
+  double gnss_pose_cov_value_uptate_period_;
+  double gnss_pose_cov_value_uptate_period_mean_;
+  double gnss_pose_cov_value_uptate_period_stddev_;
+  std::normal_distribution<double> gnss_pose_cov_value_update_period_distribution_;
+
+  double gnss_pose_cov_delay_;
+  double gnss_pose_cov_delay_mean_;
+  double gnss_pose_cov_delay_stddev_;
+  std::normal_distribution<double> gnss_pose_cov_delay_distribution_;
 
   double gnss_pose_mean_;
   double gnss_pose_stddev_;
   double gnss_pose_cov_mean_;
   double gnss_pose_cov_stddev_;
-  double gnss_pose_cov_delay_mean_;
-  double gnss_pose_cov_delay_stddev_;
 
   double imu_acc_mean_;
   double imu_acc_stddev_;
