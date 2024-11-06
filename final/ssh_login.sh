6ed61d69a30cc80dc627f46b357d08a7cf0fb471
Takashi Sato <t.sato17123@gmail.com>
2024-11-07 02:46:45 +0900

    enhance aicrc and rosrc

--- final/ssh_login.sh
+++ final/ssh_login.sh
@@ -4,4 +4,4 @@ set -e
 SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 "$SCRIPT_DIR/utils/check_aic_env.sh"
 
-ssh -t $AIC_HOSTNAME "exec bash --rcfile ~/aichallenge-2024/final/aicrc"
+ssh -t $AIC_HOSTNAME "exec bash --rcfile ~/aichallenge-2024/final/rc/aicrc"
