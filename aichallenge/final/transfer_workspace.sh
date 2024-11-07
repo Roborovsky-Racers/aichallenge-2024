cf6c47dcb0ee579dbff6b4b6a510ef6db5e4864b
Takashi Sato <t.sato17123@gmail.com>
2024-11-08 02:18:49 +0900

    add backup script

--- aichallenge/final/transfer_workspace.sh
+++ aichallenge/final/transfer_workspace.sh
@@ -7,6 +7,9 @@ SCRIPT_DIR=$(readlink -f "$(dirname "$0")")
 AIC_LOCAL_DIR="/home/$USER/aic/aichallenge-2024"
 AIC_REMOTE_DIR="/home/$AIC_USER/aichallenge-2024"
 
+# backup original aichallenge-2024 on remote
+ssh -t "$AIC_HOSTNAME" "bash -c 'cd $AIC_REMOTE_DIR/aichallenge/; if [ ! -d backup/original ]; then tar zcvf aichallenge-2024.original.tar.gz -C /home/$AIC_USER/ aichallenge-2024; mkdir -p backup/original; mv aichallenge-2024.original.tar.gz backup/original/; else echo \"original backup already exist\"; fi'" || true
+
 # transfer config files
 cd $AIC_LOCAL_DIR
 scp run_vehicle_tmux.sh run_script.tmux $AIC_HOSTNAME:$AIC_REMOTE_DIR/
@@ -15,7 +18,7 @@ scp -r aichallenge/final $AIC_HOSTNAME:$AIC_REMOTE_DIR/aichallenge/
 
 # backup workspace/src on remote
 DATE=$(date '+%Y%m%d_%H%M%S')
-ssh -t "$AIC_HOSTNAME" "bash -c 'cd $AIC_REMOTE_DIR/aichallenge/; mkdir -p backup; if [ -d workspace/src ]; then tar zcvf backup/src_$DATE.tar.gz workspace/src && rm -rf workspace/src; else echo \"workspace/src does not exist\"; fi'" || true
+ssh -t "$AIC_HOSTNAME" "bash -c 'cd $AIC_REMOTE_DIR/aichallenge/; mkdir -p backup/temp; if [ -d workspace/src ]; then tar zcvf backup/temp/src_$DATE.tar.gz workspace/src && rm -rf workspace/src; else echo \"workspace/src does not exist\"; fi'" || true
 
 # transfer workspace/src
 cd $AIC_LOCAL_DIR/aichallenge/workspace
